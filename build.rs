10a56c25d75810a620a9cbe2c556394c244dac07
AlvaroMS_25 <alvaro2ms5@gmail.com>
2021-07-01 02:11:28 +0200

    allow enabling `blocking` feature with default ones enabled

--- build.rs
+++ build.rs
@@ -1,9 +1,4 @@
 #[cfg(not(any(feature = "blocking", feature = "default")))]
 compile_error!("No features enabled, please enable one of `default` (non-blocking) or `blocking` in order to compile and run properly");
 
-#[cfg(all(feature = "blocking", feature = "default"))]
-compile_error!("Both features `default` (non-blocking) and `blocking` enabled, please disable one in order to compile and run properly, \
-if you're trying to use the blocking client, just enable the `blocking` feature and set `default-features` to `false`");
-
-
 fn main() {}
